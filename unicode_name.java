21c28d54adf7faad5c3d8fc1f847b9678fd71959
Nazik007 <99899134+Nazik007@users.noreply.github.com>
2022-05-20 22:00:02 +0300

    Create unicode_name.java

---
+++ unicode_name.java
@@ -0,0 +1,16 @@
+public class Unicode_name {
+    public static void main(String[] args) {
+        char n = '\u004E';
+        char a = '\u0061';
+        char z = '\u007A';
+        char r = '\u0072';
+        char i ='\u0069';
+        System.out.print("Hello, " + n);
+        System.out.print(a);
+        System.out.print(z);
+        System.out.print(a);
+        System.out.print(r);
+        System.out.print(i);
+        System.out.print(i);
+    }
+}
